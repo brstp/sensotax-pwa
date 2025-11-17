789c9576c1e987aede95ea04e28fa6300d81e7b1
Stefan Pettersson <stefan.pettersson@lumano.se>
2025-11-17 23:22:29 +0100

    Typo in file name.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Change the cache name when updating files to force service worker to install new assets
-const CACHE_NAME = 'sensotax-cache-v2'; 
+const CACHE_NAME = 'sensotax-cache-v3'; 
 const urlsToCache = [
   './',
   'sensotax.html',
