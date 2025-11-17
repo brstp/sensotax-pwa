d456087b4449d60f34469c08f31a4dc5b45d2068
Stefan Pettersson <stefan.pettersson@lumano.se>
2025-11-17 18:42:51 +0100

    Use English in code.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Öka CACHE_NAME till v17 eller högre!
-const CACHE_NAME = 'oltaxonomi-cache-v28';
+const CACHE_NAME = 'oltaxonomi-cache-v29';
 const urlsToCache = [
   // ... befintliga filer ...
   './', 
